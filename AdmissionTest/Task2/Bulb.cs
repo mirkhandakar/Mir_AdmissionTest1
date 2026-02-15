1bac118c9a563bfa8d1b8be2ffc1bbc896112d6c
mirkhandakar <mirkhandakar@gmail.com>
2026-02-15 21:50:42 +0600

    updated task2

--- AdmissionTest/Task2/Bulb.cs
+++ AdmissionTest/Task2/Bulb.cs
@@ -5,17 +5,16 @@ using System.Text;
 namespace Task2
 {
     public class Bulb
-    {        
+    {
         public event EventHandler On;
         public event EventHandler Off;
 
-        
         public void TurnOn()
         {
             Console.WriteLine("Bulb is on");
             On?.Invoke(this, EventArgs.Empty);
         }
-               
+
         public void TurnOff()
         {
             Console.WriteLine("Bulb is off");
--- AdmissionTest/Task2/Program.cs
+++ AdmissionTest/Task2/Program.cs
@@ -2,18 +2,28 @@
 
 class Program
 {
-   
+    // Move the event handlers HERE, inside the Program class
+    static void OnHandler(object sender, EventArgs e)
+    {
+        Console.WriteLine("Event: Bulb turned ON!");
+    }
+
+    static void OffHandler(object sender, EventArgs e)
+    {
+        Console.WriteLine("Event: Bulb turned OFF!");
+    }
+
     static void Main(string[] args)
     {
-        {            
+        {
             Bulb bulb = new Bulb();
-                        
-            bulb.On += (sender, e) => Console.WriteLine("Event: Bulb turned ON!");
-            bulb.Off += (sender, e) => Console.WriteLine("Event: Bulb turned OFF!");
-                        
+
+            // Using named methods as delegates
+            bulb.On += new EventHandler(OnHandler);
+            bulb.Off += new EventHandler(OffHandler);
+
             bulb.TurnOn();
             bulb.TurnOff();
         }
-
     }
 }
